3fb3e4ece1f9304de9d69458d4779a0b56844e17
Ivan <offlane@tut.by>
2022-11-16 22:26:57 +0300

    Отрисовка стран в UI

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import { countriesReducer } from './countries/countries-reducer';
 import { themeReducer } from './theme/theme-reducer';
 
 export const rootReducer = combineReducers({
   theme: themeReducer,
+  countries: countriesReducer,
 });
