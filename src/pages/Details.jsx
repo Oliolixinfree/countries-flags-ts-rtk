1286d2e662021763ba3b78db9949311278d48439
Ivan <offlane@tut.by>
2022-11-21 21:02:12 +0300

    Детальная страница

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,18 +4,29 @@ import { IoArrowBack } from 'react-icons/io5';
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDetails } from '../store/details/details-selectors';
+import { useEffect } from 'react';
+import { loadCountryByName } from '../store/details/details-actions';
 
 export const Details = () => {
   const { name } = useParams();
   const navigate = useNavigate();
 
-  const currentCountry = null;
+  const dispatch = useDispatch();
+  const { currentCountry, error, status } = useSelector(selectDetails);
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+  }, [name, dispatch]);
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
+      {status === 'loading' && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
   );
