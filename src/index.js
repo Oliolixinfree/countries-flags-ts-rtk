b38dc3183e53c0636ae00123e002c6ba5bbb324d
Ivan <offlane@tut.by>
2023-02-15 23:48:10 +0300

    Слайс стран async

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
 import App from './App';
-
-import { store } from './store.js';
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import * as api from './config';
 import { themeReducer } from './features/theme/theme-slice';
 import { controlsReducer } from './features/controls/controls-slice';
+import { countryReducer } from './features/countries/countries-slice';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     controls: controlsReducer,
+    countries: countryReducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) =>
