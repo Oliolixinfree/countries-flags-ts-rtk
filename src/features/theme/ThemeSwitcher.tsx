6178c551c717237e98ba7c9ffed107f31c4877a9
Ivan <offlane@tut.by>
2023-02-20 00:03:17 +0300

    Типизация переключателя темы

--- src/features/theme/ThemeSwitcher.tsx
+++ src/features/theme/ThemeSwitcher.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 import { useTheme } from './use-theme';
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import * as api from './config';
@@ -25,3 +26,7 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
